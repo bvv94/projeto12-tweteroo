3cb8a1aaccf028d66591bb1061cd0077daaeef51
bvv94 <bvdevasconcelos@gmail.com>
2023-04-07 13:06:06 -0400

    Validação de String addcionadas no sign-up e tweets

--- src/app.js
+++ src/app.js
@@ -21,6 +21,10 @@ app.post('/sign-up', (req, res) => {
         return
     }
 
+    if (typeof (username) !== 'string' || typeof (avatar) !== 'string') {
+        return res.status(400).send("Todos os campos são obrigatórios!")
+    }
+
     const newUser = {
         id: users.length + 1,
         username,
